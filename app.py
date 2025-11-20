9fc7396864dd6051483705b70c20e1bc86c1e0aa
Pmaza27 <pjmazabueu@eafit.edu.co>
2025-11-20 08:47:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
         
 
 
-broker="broker.mqqt.dashboard.com"
+broker="broker.mqqtdashboard.com"
 port=1883
 client1= paho.Client("Pablo_Maza")
 client1.on_message = on_message
