5e8d6a2a9c628d6b1002e92fc5afee422bfcfbb5
Pmaza27 <pjmazabueu@eafit.edu.co>
2025-11-20 08:54:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("GIT-HUB")
+client1= paho.Client("Pablo_Maza")
 client1.on_message = on_message
 
 
@@ -33,12 +33,12 @@ client1.on_message = on_message
 st.title("MQTT Control")
 
 if st.button('ON'):
-    act1="ON"
-    client1= paho.Client("GIT-HUB")                           
+    act1="enciende las luces"
+    client1= paho.Client("Pablo_Maza")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("pablocontrol", message)
  
     #client1.subscribe("Sensores")
     
@@ -47,12 +47,12 @@ else:
     st.write('')
 
 if st.button('OFF'):
-    act1="OFF"
-    client1= paho.Client("GIT-HUB")                           
+    act1="apaga las luces"
+    client1= paho.Client("Pablo_Maza")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("pablocontrol", message)
   
     
 else:
@@ -62,11 +62,11 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("Pablo_Maza")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("cmqtt_a", message)
+    ret= client1.publish("pablocontrol", message)
     
  
 else:
